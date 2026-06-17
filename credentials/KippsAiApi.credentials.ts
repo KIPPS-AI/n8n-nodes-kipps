6391baa3b93eb6fce34e1d4cbd3ed88943b29071
abhishekshukla9956 <1112abhishekshukla@gmail.com>
2026-06-17 12:21:41 +0530

    Updated

--- credentials/KippsAiApi.credentials.ts
+++ credentials/KippsAiApi.credentials.ts
@@ -34,7 +34,7 @@ export class KippsAiApi implements ICredentialType {
 
 	test: ICredentialTestRequest = {
 		request: {
-			baseURL: '={{"https://backend.kipps.ai"}}',
+			baseURL: '={{$credentials.baseUrl || "https://backend.kipps.ai"}}',
 			url: '/kipps/agents/',
 			method: 'GET',
 		},
